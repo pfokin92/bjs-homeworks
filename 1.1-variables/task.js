a94dad6ee91b1fc265a2143162fc2f0e41f32369
Pavel Fokin <fokinpavel2007@outlook.com>
2020-08-25 08:36:41 +0300

    First homework

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -5,23 +5,35 @@
 	3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
  */
 
-function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
-}
+ function averageMark(a, g, p) {
 
-function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
-}
+ 	let algebra = a;
+ 	let geography = g;
+ 	let physics = p;
 
-function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
-}
+ 	let result = 0;
+ 	result = (algebra + geography + physics) / 3;
+
+ 	return result;
+ }
+
+ function sayHello(userName) {
+
+ 	let myName = userName;
+ 	let message = "Привет, мир! Меня зовут ";
+
+ 	message = message + userName + ".";
+
+ 	return message;
+ }
+
+ function calculateFormula() {
+
+ 	let x = 2;
+ 	let y = 22;
+ 	let z = 0;
+
+ 	let result = x * y + 5 * z + x - 1;
+
+ 	return result;
+ }
