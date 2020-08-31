67395f5d9bb93aad92810b3cb7f63f77e7347311
Pavel Fokin <fokinpavel2007@outlook.com>
2020-08-31 22:11:05 +0300

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,40 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function checkPalindrome() {
+    let message = this.toLowerCase();
+    let result = false;
+    mess = message;
+    message = message.replace(/\s/g, '');
+    message = message.split('');
+    messageReverse = message.reverse();
+    message = message.join('');
+    messageReverse = messageReverse.join('');
+    if (mess === messageReverse) {
+        result = true;
+    }
+    return result;
+}
+console.log("sssaaa".isPalindrome())
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let count = 0;
+    let sum = 0;
+    let averageMark = 0;
+    for (let value of marks) {
+        count++;
+        sum = sum + value;
+    }
+    averageMark = sum / count;
+    return averageMark;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const dateNow = new Date();
+    const dateOfBirthday = new Date(birthday);
+    let verdict = false;
+    let diff = dateNow - dateOfBirthday;
+    let diffYear = diff / (1000 * 3600 * 24 * 365.242);
+    if (diffYear >= 18) {
+        verdict = true;
+    }
+    return verdict;
 }
