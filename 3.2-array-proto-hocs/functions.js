693e0b58854ff60982e24c48be9f6d51fdf1c739
Pavel Fokin <fokinpavel2007@outlook.com>
2020-09-29 09:32:20 +0300

    Правки в первое задание дорабатка второго

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -14,34 +14,24 @@ function getNames() {
 }
 
 function getCountReliableWeapons(durability) {
-  const weaponsMoreDurability = weapons.filter(
-    (weapon) => weapon.durability > durability
-  );
-  return weaponsMoreDurability.length;
+  return weapons.filter((weapon) => weapon.durability > durability).length;
 }
 
 function hasReliableWeapons(durability) {
-  const checkMoreDurability = weapons.some(
-    (weapon) => weapon.durability > durability
-  );
-  if (checkMoreDurability) {
-    return `Есть оружие с большей прочностью, чем ${durability}`;
-  }
-  return `Нет оружия с большей прочностью, чем ${durability}`;
+  return weapons.some((weapon) => weapon.durability > durability);
 }
 
 function getReliableWeaponsNames(durability) {
-  const weaponsMoreDurability = weapons.filter(
+  /*const weaponsMoreDurability = weapons.filter(
     (weapon) => weapon.durability > durability
-  );
-  return weaponsMoreDurability.map((weapon) => weapon.name);
+  );*/
+  return weapons.filter(
+    (weapon) => weapon.durability > durability
+  ).map((weapon) => weapon.name);
 }
 
 function getTotalDamage() {
-  const weaponsAttackArray = weapons.map((weapon) => weapon.attack);
-  return weaponsAttackArray.reduce(function (sum, attack) {
-    return sum + attack;
-  }, 0);
+  return weapons.map((weapon) => weapon.attack).reduce((sum, attack) => sum + attack, 0);
 }
 
 console.log(weapons);
@@ -50,31 +40,18 @@ console.log(getCountReliableWeapons(250));
 console.log(hasReliableWeapons(200));
 console.log(getReliableWeaponsNames(200));
 console.log(getTotalDamage());
+
 /*Необязательно*/
 function getValuestCountToSumValues(array, checkSum) {
-  let sum = 0
-  const newArray = array.map(function (item) {
-    sum = sum + item;
-    if (sum < checkSum) {
-      return item;
+  let count = 1;
+  const sum = array.reduce((sum, item, index) => {
+    sum = sum - item;
+    if (sum > 0) {
+      count++;
+      return sum;
     }
-  }).filter(item => item != undefined)
-  return newArray.length;
-}
-
-const array = [1, 2, 3, 5, 2, 7, 3, 5, 2];
-console.log(getValuestCountToSumValues(array, 10));
-console.log(getValuestCountToSumValues([1, 2, 3, 5, 2, 7, 3, 5, 2], 20));
-
-function getValuestCountToSumValues(array, checkSum) {
-  let sum = 0
-  const newArray = array.map(function (item) {
-    sum = sum + item;
-    if (sum < checkSum) {
-      return item;
-    }
-  }).filter(item => item != undefined)
-  return newArray.length;
+  }, checkSum);
+  return count;
 }
 
 const array = [1, 2, 3, 5, 2, 7, 3, 5, 2];
@@ -89,45 +66,73 @@ function sleep(milliseconds) {
 
 function sum() {
   sleep(500);
-  let args = [];
-  if (arguments.length > 1) {
-    for (let i = 0; i < arguments.length; i++) {
-      if (typeof arguments[i] === "number") {
-        args[i] = arguments[i];
-      } else if (typeof arguments[i] === "object") {
-        let lengthArgs = args.length;
-        for (let j = 0; j < arguments[i].length; j++) {
-          args[j + lengthArgs] = arguments[i][j];
-        }
-      }
-    }
-  } else if (typeof arguments[0] === "number" && arguments.length === 1) {
-    args[0] = arguments[0];
-  } else {
-    args = arguments[0];
-  }
-  return args.reduce((sum, arg) => {
-    return (sum += arg);
-  }, 0);
+  const args = arguments[0];
+  return args.reduce((sum, arg) => sum += arg, 0);
 }
 
 function compareArrays(a1, a2) {
   return a1.length == a2.length && a1.every((v, i) => v === a2[i]);
 }
 
-function memorize(fn, limit) {
-  // Вопрос в этой части. Правильно ли я понял что при вызове mSum я эти результары должен получить здесь.
-  // 
-  // args = [8, 1, 2];
-  return fn;
+let memory = [];
+
+
+
+function getMemorySum() {
+  let arg = [];
+  let cell = {};
+  let result;
+  if (arguments.length > 1) {
+    for (let i = 0; i < arguments.length; i++) {
+      if (typeof arguments[i] === "number") {
+        arg[i] = arguments[i];
+      } else if (typeof arguments[i] === "object") {
+        let lengthArgs = arg.length;
+        for (let j = 0; j < arguments[i].length; j++) {
+          arg[j + lengthArgs] = arguments[i][j];
+        }
+      }
+    }
+  } else if (typeof arguments[0] === "number" && arguments.length === 1) {
+    arg[0] = arguments[0];
+  } else {
+    arg = arguments[0];
+  }
+  if (memory.some(cell => compareArrays(arg, cell.args))) {
+    return memory.find(cell => compareArrays(arg, cell.args)).result;
+  } else {
+    result = sum(arg);
+    cell.args = arg;
+    cell.result = result;
+    memory.push(cell);
+  }
+  //console.log(memory);
+
+  return cell.result;
 }
+
+
+function memorize(fn, limit) {
+  if (limit < memory.length) {
+    memory.shift()
+  }
+  return getMemorySum;
+}
+
+
 const mSum = memorize(sum, 5);
 
 console.log(typeof mSum)
 
-console.log(mSum([8, 1, 2]))
 
-console.log(sum(8));
-console.log(sum([8, 1, 2], [8, 1, 2]));
+console.log(mSum([8, 1, 2]))
+console.log(mSum([8, 1, 3]))
+console.log(mSum([8, 1, 2]))
+console.log(mSum([8, 1, 4]))
+console.log(mSum([8, 1, 5]))
+console.log(mSum([8, 1, 4]))
+
+//console.log(sum(8));
+//console.log(sum([8, 1, 2], [8, 1, 2]));
 console.log(compareArrays([8, 1, 2], [8, 1, 2]));
 console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4]));
