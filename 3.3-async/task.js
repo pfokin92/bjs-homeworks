fd58b6f5aef2a283f63bdada146ccb03aae92d57
Pavel Fokin <fokinpavel2007@outlook.com>
2020-10-25 11:16:09 +0300

    Первая версия

---
+++ 3.3-async/task.js
@@ -0,0 +1,101 @@
+class AlarmClock{
+    constructor (){
+        this.alarmCollection = [];
+        this.timerId;
+    }
+    addClock(time, callback, id) {
+        if(id === undefined)
+        {
+            throw new Error('Невозможно индефицировать будильник.Параметр id не передан');
+        }
+        if(this.alarmCollection.some(element => element.timerId===id))
+        {
+            return console.error('Будильник с таким id существует')
+        }
+        const element = {};
+        element.time = time;
+        element.callback=callback;
+        element.timerId=id;
+        this.alarmCollection.push(element);
+    }
+    removeClock(id){
+        const find = this.alarmCollection.findIndex(element => element.timerId===id);
+        if (find === undefined)
+        {
+            return console.log('Будильник не найден');
+        }
+        return this.alarmCollection.splice(find,1);
+    }
+    getCurrentFormattedTime(){
+        const now = new Date();
+        return `${now.getHours()}:${now.getMinutes()}`;
+    }
+    start(){
+        this.identifier = setInterval(()=>{
+            return this.alarmCollection.forEach(element => this.checkClock(element.time, element.callback))
+        }
+            ,60000);
+        
+    }
+    checkClock(time, callback){
+        const timeNow = this.getCurrentFormattedTime();
+        if( time === timeNow){
+            return callback();
+        }
+    }
+    stop(){
+        clearInterval(this.identifier);
+
+    }
+    printAlarms(){
+        console.log(`Печать всех будильников в количестве:${this.alarmCollection.length}`)
+        this.alarmCollection.forEach(element => {
+            console.log(`Будильник №${element.timerId} заведен на ${element.time}`)
+        });
+    }
+    clearAlarms(){
+        clearInterval(this.identifier);
+        this.alarmCollection = [];
+    }
+}
+
+let phoneAlarm = new AlarmClock();
+
+phoneAlarm.addClock("11:16", ()=> console.log("Пора вставать"),1);
+phoneAlarm.addClock("11:14", ()=> console.log("Вставай, а то проспишь!"),2);
+phoneAlarm.addClock("11:15", ()=> console.log("Давай, вствай уже!"),3);
+phoneAlarm.addClock("11:13", ()=> console.log("Да ты задрал уже!"),4);
+phoneAlarm.addClock("11:11", ()=> console.log("Подъем!"),5);
+phoneAlarm.addClock("11:10", ()=> console.log("Я верю в тебя!"),6);
+phoneAlarm.addClock("11:12", ()=> console.log("Вставай!"),7);
+phoneAlarm.addClock("11:09", ()=> console.log("Вставай!"),8);
+
+
+phoneAlarm.printAlarms();
+
+console.log(phoneAlarm.alarmCollection);
+
+
+
+phoneAlarm.removeClock(2);
+
+
+
+//phoneAlarm.addClock("09:01", ()=> console.log("Пора вставать"));
+
+
+
+
+console.log(phoneAlarm.alarmCollection);
+
+
+phoneAlarm.start();
+//setTimeout(phoneAlarm.stop(), 15000)
+
+//phoneAlarm.clearAlarms();
+
+console.log(phoneAlarm.alarmCollection);
+
+phoneAlarm.checkClock("20:48", ()=>console.log("Hi world!"))
+
+
